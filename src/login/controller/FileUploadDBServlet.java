7b7edee94e3f736329b179240fbfa00afaafa8c8
Dawnchan1997 <37586533+Dawnchan1997@users.noreply.github.com>
2018-12-23 21:58:26 +0800

    register fuc
    register

---
+++ src/login/controller/FileUploadDBServlet.java
@@ -0,0 +1,81 @@
+package login.controller;
+import login.entity.User;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+ 
+@WebServlet("/uploadServlet")
+@MultipartConfig(maxFileSize = 16177215)    // upload file's size up to 16MB
+public class FileUploadDBServlet extends HttpServlet {
+     
+    // database connection settings
+    private String dbURL = "jdbc:mysql://localhost:3306/System?serverTimezone=GMT%2B8";
+    private String dbUser = "root";
+    private String dbPass = "fcncdn";
+
+    private User mUser;
+     
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        InputStream inputStream = null; // input stream of the upload file
+        // obtains the upload file part in this multipart request
+        Part filePart = request.getPart("photo");//ͼƬ��Ӧ�ı�ǩ�����޸ĵ����õģ�
+        if (filePart != null) {
+            // obtains input stream of the upload file
+            inputStream = filePart.getInputStream();
+        }
+         
+        Connection conn = null; // connection to the database
+        String message = null;  // message will be sent back to client
+        mUser = (User) request.getSession().getAttribute("USER_SESSION");
+        String username = mUser.getUsername();
+         
+        try {
+            // connects to the database
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+ 
+            // constructs SQL statement
+            String sql = "update user set photo=? where username=?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(2,username);
+            if (inputStream != null) {
+                // fetches input stream of the upload file for the blob column
+                statement.setBlob(1, inputStream);
+            }
+ 
+            // sends the statement to the database server
+            int row = statement.executeUpdate();
+            if (row > 0) {
+                message = "File uploaded and saved into database";
+            }
+        } catch (SQLException ex) {
+            message = "ERROR: " + ex.getMessage();
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                // closes the database connection
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+             
+            // forwards to the message page
+            getServletContext().getRequestDispatcher("/main.jsp").forward(request, response);
+        }
+    }
+}
